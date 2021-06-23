79be554ff55502c5d93c8fa7db645297fc33c5a0
Théo <theo35420@gmail.com>
2021-06-23 09:56:20 +0200

    first commit

---
+++ myapp/routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var CryptoJS = require("crypto-js");
+
+const dumb_data = require('../dumb_data.json')
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Victim Website' });
+});
+
+router.post('/connected', function(req, res, next) {
+  var hash = CryptoJS.SHA256(req.body.password);
+  var hash = hash.toString(CryptoJS.enc.Base64);
+    
+  if ((dumb_data.username == req.body.username) && (dumb_data.password == hash)) {
+    res.render('connected', { username: req.body.username});
+  } else {
+    res.render('index', { title: 'Victim Website', error: 'Invalid credentials' });
+  }
+});
+
+module.exports = router;
